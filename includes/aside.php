81a2f205ba75796b26749dbc0ef0409f46193421
Florin Mazilu <maziluflorin84@gmail.com>
2017-08-06 19:06:25 +0300

    Initial commit

---
+++ includes/aside.php
@@ -0,0 +1,8 @@
+<aside>
+    <div class="widget">
+        <h2>User</h2>
+        <div class="inner">
+            User information
+        </div>
+    </div>
+</aside>
---
+++ register.php
@@ -0,0 +1,6 @@
+<?php include 'includes/overall/header.php';?>
+
+    <h1>Register</h1>
+    <p>Just a template</p>
+
+<?php include 'includes/overall/footer.php';?>
